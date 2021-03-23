dc4c1df01017f4fc4d335e3e58c109ae3be8798c
Ahmed Elwakil <35235801+AhmedElwakil2004@users.noreply.github.com>
2021-03-23 19:57:42 +0200

    vendor path fix

--- evolution_mido.mk
+++ evolution_mido.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/mido/device.mk)
 
 # Inherit some common AEX stuff.
-$(call inherit-product, vendor/evolution/common.mk)
+$(call inherit-product, vendor/evolution/config/common.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
